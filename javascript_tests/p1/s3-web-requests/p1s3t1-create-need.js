f71a934f39a442588cdf5ffd367e84f5d29091d4
Aldian Fazrihady <mobile@aldian.net>
2020-03-29 09:25:40 +0700

    p1s3t3 create need

---
+++ javascript_tests/p1/s3-web-requests/p1s3t1-create-need.js
@@ -0,0 +1,36 @@
+const FirebaseAuth = require('../../../includes/firebase_auth.js');
+const configs = require('../../configs');
+const querystring = require('querystring');
+const axios = require('axios')
+
+var firebase_auth = new FirebaseAuth();
+var request_time = 0;
+var request_url = "https://p1s3-web-requests-dot-aqueous-choir-160420.appspot.com/p1s3t1-create-need";
+
+var params = {
+    p1s3_firebase_email: configs.user_email,
+    p1s3_token: '',
+    p1s3t1_name: 'Mask',
+    p1s3t1_requirements: 'N95 Mask for medical operations',
+};
+
+var verify_token_success_callback = function(token) {
+  params['p1s3_token'] = token;
+  request_time = new Date().getTime();
+
+  axios.post(request_url, querystring.stringify(params))
+    .then(function (response) {
+      let time_taken = new Date().getTime() - request_time;
+      console.log(`Time took by request to complete is: ${time_taken/1000} seconds`);
+      console.log(`Test passed: Response Success Code: ${response.status}`);
+      console.log(`Test passed: Response Data: ${JSON.stringify(response.data)}`);
+    })
+    .catch(function (error) {
+      let time_taken = new Date().getTime() - request_time;
+      console.log(`Time took by request to complete is: ${time_taken/1000} seconds`);
+      console.log(`There was an error while performing this operation. Error: ${error.response.status}`);
+      console.log(`Test passed: Response Data: ${JSON.stringify(error.response.data)}`);
+    });
+}
+
+firebase_auth.loginUser(configs.user_email, configs.user_password, verify_token_success_callback)
