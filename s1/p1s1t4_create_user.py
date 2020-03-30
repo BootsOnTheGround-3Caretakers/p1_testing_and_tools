7714a04b207f0f9bb69b01a2c9e6e83f8cf0642f
Kevin Stacey <kevin@nova-wd.com>
2020-03-30 00:51:36 -0500

    got firebase replication working

--- s1/p1s1t4_create_user.py
+++ s1/p1s1t4_create_user.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals, print_function
 import requests
 import json
+import time
 
 s4t1_task_sequence_list = 'p1s4t1_task_sequence_list'
 s4t1_api_key = 'p1s4t1_api_key'
@@ -11,8 +12,11 @@ params['transaction_user_uid'] = "1"
 service0 = {}
 service0['name'] = "p1s1t4-create-user"
 service0['PMA'] = {
-    'p1s1t4_first_name': "Mickey",
-    'p1s1t4_last_name': "Mouse",
+    'p1s1t4_first_name': "first name 3/29",
+    'p1s1t4_last_name': "last_name",
+    'p1s1t4_phone_number': str(round(time.time())),
+    'p1s1t4_firebase_uid': "firebase_uid",
+
 }
 
 data = {
