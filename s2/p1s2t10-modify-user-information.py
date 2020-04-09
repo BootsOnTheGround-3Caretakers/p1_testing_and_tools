19cf6b75da862c5bf4fb593e5c75d3819f01b85b
Aldian Fazrihady <mobile@aldian.net>
2020-04-09 20:26:49 +0700

    p1s2t10 test

--- s2/p1s2t10-modify-user-information.py
+++ s2/p1s2t10-modify-user-information.py
@@ -7,39 +7,52 @@ from datetime import datetime
 s4t1_task_sequence_list = 'p1s4t1_task_sequence_list'
 s4t1_api_key = 'p1s4t1_api_key'
 
+now = datetime.now()
+time_str = now.strftime("%m%d%H%M%S")
 
-today = datetime.today().strftime('%m-%d')
 test_name = "p1s2t10"
-test_today = test_name + "-" + today
-params = {}
-params['transaction_user_uid'] = "1"
-
-
-#create user
-service0 = {}
-service0['name'] = "p1s1t4-create-user"
-service0['PMA'] = {
-    'p1s1t4_first_name': test_today,
-    'p1s1t4_last_name': test_today,
-    'p1s1t4_phone_number': str(round(time.time())),
-    'p1s1t4_firebase_uid': test_today + str(round(time.time())),
+params = {
+    'transaction_user_uid': '1',
 }
-service0['RSA'] = {'p1s2t10_user_uid': 'uid'}
 
-service1 = {}
-service1['name'] = "p1s2t10-modify-user-information"
-service1['PMA'] = {
-    "p1s2t10_first_name": test_today + str(round(time.time())),
-    "p1s2t10_last_name": test_today + str(round(time.time())),
-    "p1s2t10_country_uid": "US",
-    "p1s2t10_region_uid": "0WtwHTADzxNxdEogI3ZX|1585555853",
-    "p1s2t10_area_uid": "0072xmlHlCgII3DmSG88|1585562994"
-
-}
+services = [{
+    'name': "p1s1t4-create-user",
+    'PMA': {
+        'p1s1t4_first_name': "firstname_{}_{}".format(test_name, time_str),
+        'p1s1t4_last_name': "lastname_{}_{}".format(test_name, time_str),
+        'p1s1t4_phone_number': str(round(time.time())),
+        'p1s1t4_firebase_uid': "firebase_{}_{}".format(test_name, time_str),
+        'p1s1t4_email_address': "email_{}_{}@example.com".format(test_name, time_str),
+    },
+    'RSA': {
+        'p1s2t10_user_uid': 'uid'
+    }
+}, {
+    'name': "p1s2t10-modify-user-information",
+    'PMA': {
+        "p1s2t10_first_name": "firstname2_{}_{}".format(test_name, time_str),
+        "p1s2t10_last_name": "lastname2_{}_{}".format(test_name, time_str),
+        "p1s2t10_country_uid": "US",
+        "p1s2t10_region_uid": "0WtwHTADzxNxdEogI3ZX|1585555853",
+        "p1s2t10_area_uid": "0072xmlHlCgII3DmSG88|1585562994",
+        'p1s2t10_phone_number': str(int(time.time())),
+        'p1s2t10_phone_texts': 'aaa',
+        'p1s2t10_phone_2': str(int(time.time())),
+        'p1s2t10_emergency_contact': str(int(time.time())),
+        'p1s2t10_home_address': 'address {} {}'.format(test_name, time_str),
+        'p1s2t10_email_address': "email_{}_{}@example.com".format(test_name, time_str),
+        'p1s2t10_firebase_uid': "firebase_{}_{}".format(test_name, time_str),
+        'p1s2t10_description': 'description {} {}'.format(test_name, time_str),
+        'p1s2t10_preferred_radius': '1',
+        'p1s2t10_account_flags': 'a',
+        'p1s2t10_location_cord_lat': '41.8336474',
+        'p1s2t10_location_cord_long': '-87.8723897',
+    }
+}]
 
 data = {
     s4t1_api_key: 'BootsOnTheGround',
-    s4t1_task_sequence_list: json.dumps([service0, service1]),
+    s4t1_task_sequence_list: json.dumps(services),
 }
 
 return_value = requests.post(
